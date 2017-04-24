85ffb772759943cafbe9bd85fe1db7c8e93a5fc9
Damian Krzeminski <pirxpilot@code42day.com>
2017-04-24 15:45:44 -0700

    add LOADER_STRATEGY to config
    also add Event support to config - one can now register for config
    changes provided they are effected using new `set` method

--- src/util/config.js
+++ src/util/config.js
@@ -1,10 +1,19 @@
 'use strict';
 
+const { Evented } = require('./evented');
 
-const config = {
+const config = new Evented();
+
+config.set = function set(c) {
+    Object.assign(config, c);
+    config.fire('change', config);
+};
+
+config.set({
     API_URL: 'https://api.mapbox.com',
     REQUIRE_ACCESS_TOKEN: true,
-    ACCESS_TOKEN: null
-};
+    ACCESS_TOKEN: null,
+    LOADER_STRATEGY: 'network-only'
+});
 
 module.exports = config;
