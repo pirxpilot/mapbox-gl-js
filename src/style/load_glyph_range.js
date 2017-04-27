013b3776e4fc6a51d4836c54b6a3698cd6e49f35
Damian Krzeminski <pirxpilot@code42day.com>
2017-04-26 22:11:06 -0700

    use loader for fonts
    it makes it possible to cache fonts and use fonts when off-line

--- src/style/load_glyph_range.js
+++ src/style/load_glyph_range.js
@@ -1,33 +1,31 @@
 'use strict';
 
 const { normalizeGlyphsURL } = require('../util/mapbox');
-
-const { getArrayBuffer } = require('../util/ajax');
+const makeLoader = require('../util/loader');
+const config = require('../util/config');
 const parseGlyphPBF = require('./parse_glyph_pbf');
 
+module.exports = loadGlyphRange;
 
-module.exports = function (fontstack,
-                           range,
-                           urlTemplate,
-                           callback) {
+function loadGlyphRange(fontstack, range, urlTemplate, callback) {
     const begin = range * 256;
     const end = begin + 255;
 
     const url = normalizeGlyphsURL(urlTemplate)
-            .replace('{fontstack}', fontstack)
-            .replace('{range}', `${begin}-${end}`);
+        .replace('{fontstack}', fontstack)
+        .replace('{range}', `${begin}-${end}`);
 
-    getArrayBuffer({ url }, (err, response) => {
-        if (err) {
-            callback(err);
-        } else if (response) {
-            const glyphs = {};
+    const loader = makeLoader(config.LOADER_STRATEGY);
+    loader({ url, fontstack, range }, done);
 
-            for (const glyph of parseGlyphPBF(response.data)) {
+    function done(err, { data } = {}) {
+        if (err) return callback(err);
+        const glyphs = {};
+        if (data) {
+            for (const glyph of parseGlyphPBF(data)) {
                 glyphs[glyph.id] = glyph;
             }
-
-            callback(null, glyphs);
         }
-    });
-};
+        callback(null, glyphs);
+    }
+}
