da3ab3fef051608e25ae2eef80361bb023f3b23c
Damian Krzeminski <pirxpilot@code42day.com>
2017-04-30 09:23:32 -0700

    stop using util.format
    it's browser implementation cost does not justify convenience

--- src/style-spec/error/validation_error.js
+++ src/style-spec/error/validation_error.js
@@ -1,12 +1,14 @@
 'use strict';
 
-const format = require('util').format;
+// not as nice as 'util/format' but much cheaper
+function format(str, ...params) {
+    let i = 0;
+    return str.replace(/%[sd]/ig, () => params[i++]);
+}
 
-function ValidationError(key, value /*, message, ...*/) {
-    this.message = (
-        (key ? `${key}: ` : '') +
-        format.apply(format, Array.prototype.slice.call(arguments, 2))
-    );
+function ValidationError(key, value, ...message) {
+    this.message = key ? `${key}: ` : '';
+    this.message += format(...message);
 
     if (value !== null && value !== undefined && value.__line__) {
         this.line = value.__line__;
