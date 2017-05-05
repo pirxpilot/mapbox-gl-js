754f0ebb4ffcf3db75d798d75e9451411a1ca370
Damian Krzeminski <pirxpilot@code42day.com>
2017-05-05 09:22:42 -0700

    remove canvas source

--- src/source/source.js
+++ src/source/source.js
@@ -5,8 +5,7 @@ const object = require('../util/object');
 const sourceTypes = {
     'vector': require('../source/vector_tile_source'),
     'geojson': require('../source/geojson_source'),
-    'image': require('../source/image_source'),
-    'canvas': require('../source/canvas_source')
+    'image': require('../source/image_source')
 };
 
 /*
