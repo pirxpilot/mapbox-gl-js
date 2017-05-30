aed38e44e136bbd8d15b4e86675db9231552f286
Damian Krzeminski <pirxpilot@code42day.com>
2017-05-29 20:13:28 -0700

    fix: init loader strategy for newly created workers
    we used to set loader strategy only if it was changed, it should be also
    initialized properly

--- src/source/vector_tile_source.js
+++ src/source/vector_tile_source.js
@@ -29,6 +29,7 @@ class VectorTileSource extends Evented {
             throw new Error('vector tile sources must have a tileSize of 512');
         }
 
+        this.setLoaderStrategy(config.TILE_LOADER_STRATEGY);
         config.on('change', c => this.setLoaderStrategy(c.TILE_LOADER_STRATEGY));
 
         this.setEventedParent(eventedParent);
