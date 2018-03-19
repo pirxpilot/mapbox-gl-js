d595aff370b1f0495a3b15b987ecf8b46d49d477
John Firebaugh <john.firebaugh@gmail.com>
2018-03-19 14:13:47 -0700

    Cache gl property

--- src/render/uniform_binding.js
+++ src/render/uniform_binding.js
@@ -4,7 +4,7 @@ const Color = require('../style-spec/util/color');
 
 class Uniform {
     constructor(context, location) {
-        this.context = context;
+        this.gl = context.gl;
         this.location = location;
     }
 }
@@ -18,7 +18,7 @@ class Uniform1i extends Uniform {
     set(v) {
         if (this.current !== v) {
             this.current = v;
-            this.context.gl.uniform1i(this.location, v);
+            this.gl.uniform1i(this.location, v);
         }
     }
 }
@@ -32,7 +32,7 @@ class Uniform1f extends Uniform {
     set(v) {
         if (this.current !== v) {
             this.current = v;
-            this.context.gl.uniform1f(this.location, v);
+            this.gl.uniform1f(this.location, v);
         }
     }
 }
@@ -46,7 +46,7 @@ class Uniform2fv extends Uniform {
     set(v) {
         if (v[0] !== this.current[0] || v[1] !== this.current[1]) {
             this.current = v;
-            this.context.gl.uniform2f(this.location, v[0], v[1]);
+            this.gl.uniform2f(this.location, v[0], v[1]);
         }
     }
 }
@@ -60,7 +60,7 @@ class Uniform3fv extends Uniform {
     set(v) {
         if (v[0] !== this.current[0] || v[1] !== this.current[1] || v[2] !== this.current[2]) {
             this.current = v;
-            this.context.gl.uniform3f(this.location, v[0], v[1], v[2]);
+            this.gl.uniform3f(this.location, v[0], v[1], v[2]);
         }
     }
 }
@@ -75,7 +75,7 @@ class Uniform4fv extends Uniform {
         if (v[0] !== this.current[0] || v[1] !== this.current[1] ||
             v[2] !== this.current[2] || v[3] !== this.current[3]) {
             this.current = v;
-            this.context.gl.uniform4f(this.location, v[0], v[1], v[2], v[3]);
+            this.gl.uniform4f(this.location, v[0], v[1], v[2], v[3]);
         }
     }
 }
@@ -90,7 +90,7 @@ class UniformColor extends Uniform {
         if (v.r !== this.current.r || v.g !== this.current.g ||
             v.b !== this.current.b || v.a !== this.current.a) {
             this.current = v;
-            this.context.gl.uniform4f(this.location, v.r, v.g, v.b, v.a);
+            this.gl.uniform4f(this.location, v.r, v.g, v.b, v.a);
         }
     }
 }
@@ -108,13 +108,13 @@ class UniformMatrix4fv extends Uniform {
         // unnecessary iteration:
         if (v[12] !== this.current[12] || v[0] !== this.current[0]) {
             this.current = v;
-            this.context.gl.uniformMatrix4fv(this.location, false, v);
+            this.gl.uniformMatrix4fv(this.location, false, v);
             return;
         }
         for (let i = 1; i < 16; i++) {
             if (v[i] !== this.current[i]) {
                 this.current = v;
-                this.context.gl.uniformMatrix4fv(this.location, false, v);
+                this.gl.uniformMatrix4fv(this.location, false, v);
                 break;
             }
         }
