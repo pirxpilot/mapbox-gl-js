24ccce64eacfb13e7741bbb4eaf997229747f34d
Michael Barry <msbarry@users.noreply.github.com>
2018-11-12 13:57:50 -0500

    begin touch zoom immediately when rotation disabled (#7582)

--- src/ui/handler/touch_zoom_rotate.js
+++ src/ui/handler/touch_zoom_rotate.js
@@ -51,7 +51,10 @@ function makeMovement(map, { rotationDisabled, aroundCenter }, e) {
 
         if (!gestureIntent) {
             const scale =  vector.mag() / startVector.mag();
-            if (Math.abs(1 - scale) > SIGNIFICANT_SCALE_THRESHOLD) {
+            if (rotationDisabled && scale !== 1) {
+                // when rotation is disabled, any scale change triggers the zoom gesture to start
+                gestureIntent = 'zoom';
+            } else if (Math.abs(1 - scale) > SIGNIFICANT_SCALE_THRESHOLD) {
                 gestureIntent = 'zoom';
             }
         }
