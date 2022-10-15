6fa9d36d00c57150221c1e5331db72beb3b5e9d1
Damian Krzeminski <pirxpilot@furkot.com>
2022-10-15 11:45:41 +0200

    load map worker from blob to allow for cross-origin

--- src/util/browser/web_worker.js
+++ src/util/browser/web_worker.js
@@ -3,6 +3,19 @@
 const window = require('../window');
 const config = require('../config');
 
+
+
 module.exports = function () {
-    return new window.Worker(config.WORKER_URL);
+    return new window.Worker(getWorkerUrl());
 };
+
+let workerUrl;
+
+function getWorkerUrl() {
+    if (!workerUrl) {
+        const url = new window.URL(config.WORKER_URL, window.location.origin);
+        const workerBlob = new Blob([`importScripts('${url}')`], { type: 'text/javascript' });
+        workerUrl = window.URL.createObjectURL(workerBlob);
+    }
+    return workerUrl;
+}
