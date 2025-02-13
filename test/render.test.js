e1916de735c7d5b433a1feacc8ff39a1a81884ef
Natalia Kowalczyk <melitele@code42day.com>
2025-02-12 22:04:05 -0700

    remove ajax

--- test/render.test.js
+++ test/render.test.js
@@ -1,7 +1,5 @@
 /* eslint-disable import/unambiguous, import/no-commonjs, no-global-assign */
 
-require('./stub_loader');
-
 const suite = require('./integration').render;
 const suiteImplementation = require('./suite_implementation');
 const ignores = require('./ignores.json');
