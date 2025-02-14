591973bddcebc717f6c5f5db2f95d94828ed32f6
Damian Krzeminski <pirxpilot@furkot.com>
2025-02-14 15:55:05 -0700

    lint enforce `noSelfCompare` rule

--- test/unit/util/unique_id.test.js
+++ test/unit/util/unique_id.test.js
@@ -4,7 +4,8 @@ const test = require('mapbox-gl-js-test').test;
 const uniqueId = require('../../../src/util/unique_id');
 
 test('unique_id', async t => {
-  t.ok(typeof uniqueId() === 'number', 'uniqueId');
-  t.ok(uniqueId() !== uniqueId(), 'uniqueId');
-  t.end();
+  t.assert.equal(typeof uniqueId(), 'number', 'should be a number');
+  const id1 = uniqueId();
+  const id2 = uniqueId();
+  t.assert.notEqual(id1, id2, 'uniqueId uniqueness');
 });
