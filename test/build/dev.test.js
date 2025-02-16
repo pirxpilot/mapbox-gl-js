1db569db893759db61dc27ab544d934726feb912
Damian Krzeminski <pirxpilot@furkot.com>
2025-02-16 12:09:07 -0700

    remove unused build tests

--- test/build/dev.test.js
+++
@@ -1,7 +0,0 @@
-const {test} = require('../util/mapbox-gl-js-test');
-const fs = require('fs');
-
-test('dev build contains asserts', async (t) => {
-    t.assert.ok(fs.readFileSync('dist/mapbox-gl-dev.js', 'utf8').indexOf('canary assert') !== -1);
-    t.end();
-});
