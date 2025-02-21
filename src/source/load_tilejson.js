365251bff8e2ab8e6f59934bf0ac2dea82deea3d
Damian Krzeminski <pirxpilot@furkot.com>
2025-02-20 18:51:41 -0700

    fix load_tilejson

--- src/source/load_tilejson.js
+++ src/source/load_tilejson.js
@@ -2,39 +2,34 @@ const { pick } = require('../util/object');
 const browser = require('../util/browser');
 
 module.exports = function (options, callback) {
-  function loaded(err, tileJSON) {
-    if (err) return callback(err);
-    if (tileJSON) {
-      const { resourceSets } = tileJSON;
-      if (resourceSets) {
-        if (!resourceSets.length) {
-          return callback('expected resources');
-        }
-        const { resources } = resourceSets[0];
-        if (!resources?.length) {
-          return callback('expected resources');
-        }
-        const { imageUrl, imageUrlSubdomains } = resources[0];
-        const result = Object.assign(
-          {
-            tiles: imageUrlSubdomains.map(sub => imageUrl.replace('{subdomain}', sub).replace('http:', 'https:'))
-          },
-          options
-        );
-        delete result.url;
-        return callback(null, result);
-      }
-      const result = pick(tileJSON, ['tiles', 'minzoom', 'maxzoom', 'attribution', 'mapbox_logo', 'bounds']);
-
-      if (tileJSON.vector_layers) {
-        result.vectorLayers = tileJSON.vector_layers;
-        result.vectorLayerIds = result.vectorLayers.map(layer => layer.id);
-      }
-
-      callback(null, result);
-    }
-  }
-
   // expects already loaded object, `url` property is ignored
-  browser.frame(() => loaded(null, options));
+  browser.frame(() => loaded(options));
+
+  function loaded(tileJSON) {
+    const { resourceSets } = tileJSON;
+    if (resourceSets) {
+      if (!resourceSets.length) {
+        return callback('expected resources');
+      }
+      const { resources } = resourceSets[0];
+      if (!resources?.length) {
+        return callback('expected resources');
+      }
+      const { imageUrl, imageUrlSubdomains } = resources[0];
+      const result = {
+        tiles: imageUrlSubdomains.map(sub => imageUrl.replace('{subdomain}', sub).replace('http:', 'https:')),
+        ...options
+      };
+      delete result.url;
+      return callback(null, result);
+    }
+    const result = pick(tileJSON, ['tiles', 'minzoom', 'maxzoom', 'attribution', 'bounds']);
+
+    if (tileJSON.vector_layers) {
+      result.vectorLayers = tileJSON.vector_layers;
+      result.vectorLayerIds = result.vectorLayers.map(layer => layer.id);
+    }
+
+    callback(null, result);
+  }
 };
